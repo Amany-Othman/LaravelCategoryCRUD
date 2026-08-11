7d6491523427e5a172649bfa43a6a177e9804c1e
Amany Othman <165468609+Amany-othman@users.noreply.github.com>
2026-08-11 13:39:07 +0300

    feat: integrate admin dashboard

---
+++ resources/views/admin/layouts/app.blade.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <title>@yield('title', 'Admin Dashboard')</title>
+
+    <link href="{{ asset('admin/vendor/fontawesome-free/css/all.min.css') }}" rel="stylesheet">
+
+    <link href="{{ asset('admin/css/sb-admin-2.min.css') }}" rel="stylesheet">
+
+</head>
+
+<body id="page-top">
+
+    <div id="wrapper">
+
+        <!-- Sidebar -->
+        <ul class="navbar-nav bg-gradient-primary sidebar sidebar-dark accordion" id="accordionSidebar">
+
+            <!-- Sidebar Brand -->
+            <a class="sidebar-brand d-flex align-items-center justify-content-center"
+                href="{{ route('admin.dashboard') }}">
+
+                <div class="sidebar-brand-icon rotate-n-15">
+                    <i class="fas fa-laugh-wink"></i>
+                </div>
+
+                <div class="sidebar-brand-text mx-3">
+                    Admin Dashboard
+                </div>
+
+            </a>
+
+            <hr class="sidebar-divider my-0">
+
+            <!-- Dashboard -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.dashboard') }}">
+                    <i class="fas fa-fw fa-tachometer-alt"></i>
+                    <span>Dashboard</span>
+                </a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <div class="sidebar-heading">
+                Management
+            </div>
+
+            <!-- Categories -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.categories.index') }}">
+                    <i class="fas fa-fw fa-list"></i>
+                    <span>Categories</span>
+                </a>
+            </li>
+
+            <!-- Products -->
+            <li class="nav-item">
+                <a class="nav-link" href="{{ route('admin.products.index') }}">
+                    <i class="fas fa-fw fa-box"></i>
+                    <span>Products</span>
+                </a>
+            </li>
+
+            <hr class="sidebar-divider">
+
+            <!-- Sidebar Toggler -->
+            <div class="text-center d-none d-md-inline">
+                <button class="rounded-circle border-0" id="sidebarToggle"></button>
+            </div>
+
+        </ul>
+        <!-- End Sidebar -->
+
+
+        <!-- Content Wrapper -->
+        <div id="content-wrapper" class="d-flex flex-column">
+
+            <div id="content">
+
+                <!-- Topbar -->
+                <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
+
+                    <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+                        <i class="fa fa-bars"></i>
+                    </button>
+
+                    <h5 class="mb-0">
+                        @yield('page-heading', 'Dashboard')
+                    </h5>
+
+                </nav>
+                <!-- End Topbar -->
+
+
+                <!-- Page Content -->
+                <div class="container-fluid">
+
+                    @yield('content')
+
+                </div>
+                <!-- End Page Content -->
+
+            </div>
+
+        </div>
+        <!-- End Content Wrapper -->
+
+    </div>
+
+    <!-- Scroll to Top Button -->
+    <a class="scroll-to-top rounded" href="#page-top">
+        <i class="fas fa-angle-up"></i>
+    </a>
+
+
+    <!-- JavaScript -->
+    <script src="{{ asset('admin/vendor/jquery/jquery.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
+    <script src="{{ asset('admin/vendor/jquery-easing/jquery.easing.min.js') }}"></script>
+    <script src="{{ asset('admin/js/sb-admin-2.min.js') }}"></script>
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -3,13 +3,22 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\Admin\DashboardController;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
 Route::resource('category', CategoryController::class);
-Route::resource('products', ProductController::class);   
-
+Route::resource('products', ProductController::class);
 
 Route::view('/test/products', 'products.test');
+
+Route::get('/dashboard', [DashboardController::class, 'index'])
+    ->name('admin.dashboard');
+
+Route::resource('admin/categories', CategoryController::class)
+    ->names('admin.categories');
+
+Route::resource('admin/products', ProductController::class)
+    ->names('admin.products');
